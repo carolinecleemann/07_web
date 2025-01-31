9fa9e7f4f8bcb3aff981afb8eac404ee3aed6cda
Caroline <cacl001@stud.kea.dkgit config --get user.namegit config --global user.email cacl001@stud.kea.dk>
2025-01-31 11:07:29 +0100

    add product list

---
+++ produktliste.js
@@ -0,0 +1,24 @@
+const listContainer = document.querySelector(".accesories_list_container");
+
+fetch('https://kea-alt-del.dk/t7/api/products')
+    .then((response) => response.json())
+    .then((data) => showList(data));
+
+function showList(products) {
+    console.log(products);
+    let markup = ""; 
+    products.map((product) => { markup +=`
+    
+    <div class="rød_cap">
+
+        <a href="produkt.html">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="cap_2358"></a>
+        <p>${product.brandname} <br><br> ${product.category}</p>
+          <p><strong>${product.productdisplayname}</strong>
+          <p>${product.price} kr.</p>
+        <a href="produkt.html" class="learn">read more</a>
+    </div>`
+    }).join="";
+    console.log(markup);
+    listContainer.innerHTML = markup;
+    }
